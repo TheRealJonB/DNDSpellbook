8e31d3422c9f6f70fd5a93394b2fb4c8abfe38ba
Jon Burkher <jonburkher1@gmail.com>
2026-06-15 19:28:20 -0400

    unneeded function in filters removed

--- src/shared/utils/filters.ts
+++ src/shared/utils/filters.ts
@@ -40,12 +40,3 @@ export const buildFilterCategoriesFromData = (filterData: FilterData[]): FilterC
     })),
   }));
 };
-
-export const resetFilters = (filterCategories: FilterCategory[]) => {
-  filterCategories.map((filterCategory) => (
-    filterCategory.options.map(item => ({
-      ...item,
-      isSelected: false, // Baseline dynamic state
-    }))
-  ));
-}
