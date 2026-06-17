0c2edbdda321f33a2069316fcd4588219d5db67f
Jon Burkher <jonburkher1@gmail.com>
2026-06-17 09:43:24 -0400

    did actually need one of those screens

--- app/(tabs)/library/_layout.tsx
+++ app/(tabs)/library/_layout.tsx
@@ -4,6 +4,9 @@ export default function LibraryLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+      <Stack.Screen name="[spellId]" options={{ animation: 'slide_from_right' }} />
+      {/* <Stack.Screen name="[magicItemId]" options={{ animation: 'slide_from_right' }} /> */}
+      {/* <Stack.Screen name="[classId]" options={{ animation: 'slide_from_right' }} /> */}
     </Stack>
   );
 }
