fa10c7ef268a4d758107375a5f1d476363d593d0
Jon Burkher <jonburkher1@gmail.com>
2026-06-17 09:40:27 -0400

    no longer need these screens

--- app/(tabs)/library/_layout.tsx
+++ app/(tabs)/library/_layout.tsx
@@ -4,10 +4,6 @@ export default function LibraryLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="spells" />
-      <Stack.Screen name="magic-items" />
-      <Stack.Screen name="species" />
-      <Stack.Screen name="classes" />
     </Stack>
   );
 }
